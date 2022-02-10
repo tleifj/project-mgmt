4881171bd79181f65a54323b4428312eed38c131
Travis Johnson <travis@thisisvisceral.com>
2022-02-09 21:21:32 -0800

    Pulls in projects from database and renders a simple list on projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,33 @@
-export default function projectsPage() {
+import { PrismaClient } from '@prisma/client'
+import Link from 'next/link'
+
+
+const prisma = new PrismaClient();
+
+export default function projectsPage({ projects}) {
     return (
         <div>
             <h1>Projects Page</h1>
             <p>This is the projects page</p>
+            {projects.map(project =>  (
+                    <Link key={project.id} href="/project/[id]" as={`/project/${project.id}`}>
+                        <a>
+                            <h2>{project.name}</h2>
+                            <p>{project.description}</p>
+                        </a>
+                    </Link>
+                )
+            )}
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const results = await prisma.project.findMany({});
+    const projects = results.map((project) => {
+        project.createdAt = project.createdAt.toString();
+        project.updatedAt = project.updatedAt.toString();
+        return project
+      })
+    return { props: { projects: projects } }
 }
